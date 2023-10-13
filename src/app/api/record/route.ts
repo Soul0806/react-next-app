cf11fe5de28f5b202f40872a6d1a014c7158e0ae
soul <celestiallie@gmail.com>
2023-10-13 13:01:03 +0800

    cm

--- src/app/api/record/route.ts
+++ src/app/api/record/route.ts
@@ -1,14 +1,20 @@
 import prisma from "@/utils/prismaClient"
 import { query } from '@/lib/query';
 
+function add_quote(arr: string[]) {
+  return arr.map((item: string) => {
+    return `'${item}\'`;
+  })
+}
+
 export async function POST(request: Request) {
   const res = await request.json();
   const column_string = Object.keys(res).join(',');
-  const values_string = Object.values(res).join(',');
+  const values_string = add_quote(Object.values(res)).join(',');
+
   try {
     const querySql: string = `INSERT INTO record (${column_string}) VALUES (${values_string})`;
     const values: string[] = [];
-    console.log(querySql);
     const result = await query(querySql, values);
     return Response.json(result);
   } catch (error) {
