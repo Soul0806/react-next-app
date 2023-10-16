fa213ab55820215fdd06e87e9b7a0b48459d84b9
soul <celestiallie@gmail.com>
2023-10-16 12:57:38 +0800

    cm

--- src/app/record/client/index.tsx
+++ src/app/record/client/index.tsx
@@ -1,14 +1,17 @@
 'use client'
 
 // React 
-import { useRef, useState } from "react";
+import { createContext, useContext, useRef, useState } from "react";
 
 // Client comp
 import DialogRecord from "../DialogRecord";
-import Search from "./Search";
+import Input from '@/components/form/Input';
+// import Search from "./Search";
 
 // Type
 import type { GroupData } from "../DialogRecord";
+
+export const RecordContext = createContext<any | null>(null);
 
 export type Record = {
     [key: string]: string
@@ -22,6 +25,8 @@ type Props = {
     groupData: GroupData,
     records: any[],
 }
+
+const test = 123;
 
 function groupRecord(records: Record[]) {
     const group_record: GroupRecord = {};
@@ -38,7 +43,7 @@ function groupRecord(records: Record[]) {
     return group_record;
 }
 
-function Index(props: Props) {
+export function Index(props: Props) {
 
     const { groupData, records } = props;
     const [filteredRecord, SetFilteredRecord] = useState<GroupRecord>({});
@@ -54,80 +59,84 @@ function Index(props: Props) {
 
     function onchange(e: React.ChangeEvent<HTMLInputElement>) {
 
+        const search = e.target.value;
+
         if (refView.current) {
             refView.current.addEventListener('animationend', function () {
                 this.classList.remove('record__view__records');
             })
         }
 
-        const search = e.target.value;
-        if (search != "") {
-            const result: any[] = records.filter(item => {
-                return item.spec.match(search) || item.note.match(search);
-            })
-            const group = groupRecord(result);
-            const keys = Object.keys(group).reverse();
-            SetFilteredRecord(group);
-            setKeys(keys);
-
-            if (refView.current) {
-                refView.current.classList.add('record__view__records');
-            }
+        if (search == '') {
+            setKeys([]);
+            return;
         }
+
+        const result: any[] = records.filter(item => {
+            return item.spec.match(search) || item.note.match(search);
+        })
+        const group = groupRecord(result);
+        const keys = Object.keys(group).reverse();
+        SetFilteredRecord(group);
+        setKeys(keys);
+
+        if (refView.current) {
+            refView.current.classList.add('record__view__records');
+        }
+
     }
     return (
-        // <div>123</div>
-        <div className="record">
-            <div className="wrapper">
-                <div className="record__operate">
-                    <DialogRecord groupData={groupData} lastId={lastId} />
-                    <div className="record__operate__input">
-                        <Search onchange={onchange} />
-                    </div>
-                    {/* <div className="record__operate__groupview" style={toggleGroupViewShow}>
-                            <div className="groupview__wrapper">
-                                <div className="groupview__menu">
-                                    <svg className="groupview__close" style={toggleSearchClose} onClick={searchDelete} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
-                                        <path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z"></path>
-                                    </svg>                                
-                                </div>
-                                <GroupView filteredSale={filteredSale} groupViewProps={groupViewProps} />
-                            </div>
-                        </div> */}
-                </div>
-                {/* <input id="datepicker" /> */}
-                <section className="record__view">
-                    <div ref={refView}>
-                        {keys.map((key, index) => (
-                            <div key={index}>
-                                <div>{key}</div>
-                                <ul>
-                                    {filteredRecord[key].map(item => (
-                                        <li key={item.id}>
-                                            <div>{item.spec} -- {item.note}</div>
-                                        </li>
-                                    )
-                                    )}
-                                </ul>
-                            </div>
-                        ))}
-                    </div >
-                </section>
-                {/* <section className="record__view">
-                        <div className="record__overview__view">
-                            {!isEmpty(salesState.dbSale) &&
-                                <>
-                                    {salesState.dbSale.map(sale => {
-                                        return <Sale key={sale.id} sale={sale} salesState={salesState} remove={remove} />
-                                    })
-                                    }
-                                </>
-                            }
+        <RecordContext.Provider value={{ setKeys }}>
+            <div className="record">
+                <div className="wrapper">
+                    <div className="record__operate">
+                        <DialogRecord groupData={groupData} lastId={lastId} />
+                        <div className="record__operate__input">
+                            <Input onchange={onchange} placeholder="搜尋規格" />
                         </div>
-                    </section> */}
+                        {/* <div className="record__operate__groupview" style={toggleGroupViewShow}>
+                                <div className="groupview__wrapper">
+                                    <div className="groupview__menu">
+                                        <svg className="groupview__close" style={toggleSearchClose} onClick={searchDelete} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
+                                            <path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z"></path>
+                                        </svg>
+                                    </div>
+                                    <GroupView filteredSale={filteredSale} groupViewProps={groupViewProps} />
+                                </div>
+                            </div> */}
+                    </div>
+                    {/* <input id="datepicker" /> */}
+                    <section className="record__view">
+                        <div ref={refView}>
+                            {keys.map((key, index) => (
+                                <div key={index}>
+                                    <div>{key}</div>
+                                    <ul>
+                                        {filteredRecord[key].map(item => (
+                                            <li key={item.id}>
+                                                <div>{item.spec} -- {item.note}</div>
+                                            </li>
+                                        )
+                                        )}
+                                    </ul>
+                                </div>
+                            ))}
+                        </div >
+                    </section>
+                    {/* <section className="record__view">
+                            <div className="record__overview__view">
+                                {!isEmpty(salesState.dbSale) &&
+                                    <>
+                                        {salesState.dbSale.map(sale => {
+                                            return <Sale key={sale.id} sale={sale} salesState={salesState} remove={remove} />
+                                        })
+                                        }
+                                    </>
+                                }
+                            </div>
+                        </section> */}
+                </div >
             </div >
-        </div >
+        </RecordContext.Provider>
     );
 }
-
-export default Index;
--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -1,26 +1,38 @@
 'use client'
 
 // React
-import React, { FC, forwardRef, useRef } from "react";
+import React, { FC, forwardRef, useContext, useRef } from "react";
+
+// Context
+import { RecordContext } from "@/app/record/client/Index";
+
+// THird party
+import { isEmpty } from "lodash";
 
 // Css 
 import style from '@/styles/comps/form.module.css';
 
 type Props = {
     label?: string,
+    placeholder?: string,
     onchange: (e: React.ChangeEvent<HTMLInputElement>) => void,
 }
 
 const FormText = (props: Props) => {
     const { label, onchange, ...inputs } = props;
-    
+
     // ref 
     const refInput = useRef<HTMLInputElement>(null);
 
+    // Use context 
+    const { setKeys } = useContext(RecordContext);
+    console.log(setKeys);
+
     function clearInput() {
-        if(refInput.current) {
+        if (refInput.current) {
             refInput.current.value = "";
         }
+        setKeys([]);
     }
     return (
         <div className={style["input-wrapper"]}>
@@ -28,7 +40,8 @@ const FormText = (props: Props) => {
                 <input ref={refInput} className={style["input-elem"]} onChange={onchange} {...inputs}
                 />
             </label>
-            <button onClick={clearInput} className={style["input-clear"]}>&times;</button>
+            {!isEmpty(refInput?.current?.value) && <button onClick={clearInput} className={style["input-clear"]}>&times;</button>}
+
             <div className={style["input-underline"]}></div>
         </div>
     )
